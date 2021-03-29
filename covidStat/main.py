7509532792d64dcede93f1edaa9d32eb4b983b2b
Szalamandra <Szalamandra@users.noreply.github.com>
2021-03-30 00:10:20 +0200

    holnaptol wxgalde

--- covidStat/main.py
+++ covidStat/main.py
@@ -14,38 +14,64 @@ URL = "https://news.google.com/covid19/map?hl=hu&gl=HU&ceid=HU%3Ahu&mid=%2Fm%2F0
 
 #html_text = requests.get(URL).content  ez akkor ha csak a requestset importalom
 class FoCucc:
-    FAJL = "mentettData.txt"
-    
-    def fajlNyitasOlvasasVagyIras():
+    FAJL = "mentettTestData.txt"
+    FAJLOLVASNI="mentettData.txt"
+    def fajlNyitasFelulIrasra():
         with open((FoCucc.FAJL), "r", encoding="utf-8") as fr:
             fr = fr.read().replace("\n", ":").split(":")
             for x in fr[:-4]:
                 ujF, ujH,mN = fr[-4], fr[-3], fr[-1]
                 utsoNapiAdat = NapiAdat(ujF, ujH, mN)
-            #return utsoNapiAdat.maiNapVan
-        if (utsoNapiAdat.maiNap):
-            mentes(utsoNapiAdat)
-        else:
-            felulIras(utsoNapiAdat)
-    
-    def mentes(ujObject):
-        ujObject=NapiAdat.napiAdatKeres()
-        with open(FAJL, "a", encoding="UTF-8") as fa:
-            line = ":".join(ujObject.ujFertozott, ujObject.napiHalott, ujObject.maiNap, ujObject.mentesNap)
-            fa.write(line)
-            fa.close()
-            #return utsoNapiAdat.maiNapVan
+            print(utsoNapiAdat.maiNapVan)
+            return utsoNapiAdat.maiNapVan
 
-    def felulIras(ujObject):
-        ujObject=NapiAdat.napiAdatKeres()
-        with open(FAJL, "w", encoding="UTF-8") as fw:
-            fr = fr.read().replace("\n", ":").split(":")
-            for x in fr[:-4]:
-                fr[-4] = ujObject.ujFertozott
-                fr[-3] = ujObject.napiHalott
-            fr.write()
-            fr.close()
+    def adatIras(fajlNyitasFelulIrasra = None, obj=None):
+        if obj == None:
+            raise Exception
+            print("vm hiba az adatlekérésben")
+        #print("fajlolvassasra: "+str(fajlNyitasFelulIrasra)+ "obj: "+str(obj))
+        if fajlNyitasFelulIrasra == None:
+            fajlNyitasFelulIrasra = True
+        if fajlNyitasFelulIrasra == True:
+            obj.felulIras(obj)
+            print("felülírtam")
+        else:
+            obj.mentes(obj)
+            print("mentettem")
+    
+
             #return utsoNapiAdat.maiNapVan
+    def adatOlvasas():
+        objectList=[]
+        with open(FoCucc.FAJLOLVASNI, 'rt', encoding='utf-8') as fr:
+            lines = fr.readlines()
+            #utolsó 5 sorral akarok foglalkozni:
+            ujLinesReverse = lines.copy()
+            ujLinesReverse = ujLinesReverse[-1:-6:-1]
+            for lines in range(5):
+                az, nF, nH, nD, mN = ujLinesReverse[lines].replace('\n', '').split(':')
+                uj = NapiAdat(az, nF, nH, mN)
+                #print(uj)
+                objectList.append(uj)
+
+            #print(str(objectList))
+            return objectList
+    #tablazathoz majd:        
+    def kiIrFertozottek(objList):
+        for i in range(5):
+            print(objList[i].ujFertozott)
+    def kiIrHalottak(objList):
+        for i in range(5):
+            print(objList[i].napiHalott)
+    #ertekeles
+    def ertekeles(objList):
+        szamolo=0
+        for i in range(5):
+            if int(objList[i].napiHalott)<100: szamolo+=1
+        if szamolo == 5: print("Eljött az ünneplés diadala!")
+        elif szamolo > 0 and szamolo < 5: print("Lassan indulnék a boltba...")
+        else: print("Sajnos még nyaral Dionüszosz...")
+
 
         
 
@@ -53,15 +79,16 @@ class NapiAdat:
     COUNTER = 0
     maiNap=datetime.now()
 
-    def __init__(self, ujFertozott, napiHalott, mentesNap):
+    def __init__(self, az, ujFertozott, napiHalott, mentesNap):
         self.ujFertozott = ujFertozott
         self.napiHalott = napiHalott
-        self.COUNTER += 1
+        self.az = az
+        az=NapiAdat.COUNTER+1
         self.mentesNap=mentesNap
-        self.maiNapVan= int(NapiAdat.maiNap.day)==int(self.mentesNap)
+        self.maiNapVan= str(NapiAdat.maiNap.day)==self.mentesNap
         
     def __str__(self):
-        return f"{self.COUNTER}:{self.ujFertozott}:{self.napiHalott}:{self.mainapStr()}:{self.mentesNap}"
+        return f"{self.az}:{self.ujFertozott}:{self.napiHalott}:{self.mainapStr()}:{self.mentesNap}"
 
     def mainapStr(self):
         return f"{self.maiNap.year} {self.maiNap.month} {self.maiNap.day}"
@@ -82,11 +109,45 @@ class NapiAdat:
             ujFertozott = str(napiDataList[0])[8:-9]        #.strip("<strong>") de nemjo a zarotag miatt   # str mert a type is bs4 class element
             napiHalott = str(napiDataList[1])[8:-9] 
         #print(f"{napiHalott} es a fert {ujFertozott}")
-
-        ujNapi = NapiAdat(ujFertozott, napiHalott)
+        ma = str(NapiAdat.maiNap.day)
+        az=NapiAdat.COUNTER
+        ujNapi = NapiAdat(az,ujFertozott, napiHalott, ma )
         return ujNapi
+    
+    def mentes(self,ujObject=None):
+        if ujObject == None:
+            raise Exception
+        else:    
+            with open(FoCucc.FAJL, "a", encoding="UTF-8") as fa:
+                line = ":".join(["\n"+str(ujObject.az), ujObject.ujFertozott, ujObject.napiHalott, ujObject.mainapStr(), str(ujObject.mentesNap)])
+                fa.write(line)
+                fa.close()
+                #return utsoNapiAdat.maiNapVan
+
+    def felulIras(self, ujObject=None):
+        if ujObject == None:
+            raise Exception
+        else:
+            with open(FoCucc.FAJL, "a+", encoding="UTF-8") as fw:
+                fw.seek(0)
+                lines = fw.readlines()
+                utsosorCharSzam=len(lines[len(lines) - 1])
+                utsoSor = lines[len(lines) - 1].split(":")  #listat ad vissza az utsosor elemeivel
+                utsosor[0]=ujObject.az
+                utsoSor[1] = ujObject.ujFertozott
+                utsoSor[2] = ujObject.napiHalott
+                regiObject=NapiAdat(utsosor[0],utsoSor[1],utsoSor[2],ujObject.mentesNap)
+
+                print('regi: {0}, utsosor: {1}'.format(regiObject, utsoSor))
+                fw.seek(regiObject.az)
+                fw.write(str(regiObject))
+
+                fw.close()
 
 
-
-#NapiAdat.napiAdatKeres()
-FoCucc.fajlNyitasOlvasasVagyIras()
+#obj=NapiAdat.napiAdatKeres()
+#FoCucc.adatIras(FoCucc.fajlNyitasFelulIrasra(), obj)
+adatObjLista = FoCucc.adatOlvasas()
+#FoCucc.kiIrFertozottek(adatObjLista)
+#FoCucc.kiIrHalottak(adatObjLista)
+FoCucc.ertekeles(adatObjLista)
